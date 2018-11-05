94329d9112be20e6c3dfd6b7bba5e90d5fb4a323
Windows <wittysreenath@gmail.com>
2018-11-05 07:16:43 +0000

    fourth COmmit

--- hello.java
+++ hello.java
@@ -1,10 +1,10 @@
 public class hello{
 
-	public static void main(String[] args){
-	
-		for(int i=1;i<=10;i++){
+        public static void main(String[] args){
 
-			System.out.println("Hello World ..."+i);
+                for(int i=1;i<=10;i++){
+
+                        System.out.println("Hello World ..."+i);
 
 }
 }
